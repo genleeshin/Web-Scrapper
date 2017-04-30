3f91b2982220003e38155ca97eaf87f3e01e5703
Nasim Mahmud <gen.lee.shin@gmail.com>
2017-05-01 00:26:08 +0100

    promise accumulation done

--- Crawler.js
+++ Crawler.js
@@ -15,31 +15,34 @@ module.exports = class Crawler{
 		this.x.timeout(5000).throttle(1,5000);
 	}
 
-	static crawl(urls){
+	static crawl(store, resolve, reject){
 
 		let instance = new Crawler();
 
+		instance.store = store;
+
 		let funcs = [];
 
-		urls.forEach( url => funcs.push(
+		store.urls.forEach( url => funcs.push(
 			function(callback){
-				instance.start(url, callback);
+				instance.start(url, store.op, callback);
 			}
 		));
 
 		async.series(funcs, function(err, result){
 			//console.log(result);
 			console.log('all done');
+			resolve(store.name);
 
 		});
 	}
 
-	start(url, callback){
+	start(url, op, callback){
 
 		let self = this;
 
-		this.x(url, 'li', [{title: 'h2'}])
-		.paginate('a@href')
+		this.x(url, op.root, [op.at])
+		.paginate(op.paginate)
 		.limit(5)( (err, obj) => {
 			self.processData(err, obj);
 			callback(err, obj);
--- index.js
+++ index.js
@@ -12,8 +12,60 @@ var urls2 = [
 	"http://localhost:3000/index.php?cat=skirt"
 ];
 
-crawler.crawl(urls);
+let stores = [
+	{
+		name: 'argos',
 
-crawler.crawl(urls2);
+		urls: urls,
+
+		op: {
+			root: 'li',
+
+			at: {
+				title: 'h2'
+			},
+
+			paginate: 'a@href'
+		}
+
+	},
+
+	{
+		name: 'asos',
+
+		urls: urls2,
+
+		op: {
+			root: 'li',
+
+			at: {
+				title: 'h2'
+			},
+
+			paginate: 'a@href'
+		}
+	}
+
+];
+
+//store all promises
+let p = [];
+
+stores.forEach(store => {
+
+	p.push(
+		new Promise(
+			(resolve, reject) => {
+
+				crawler.crawl(store, resolve, reject);
+			}
+		)
+	);
+});
+
+Promise.all(p).then( data => {
+	console.log(data);
+	console.log('done all promises');
+});
 
 
