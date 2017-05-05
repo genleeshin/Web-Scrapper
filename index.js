8184d326bb25f80f1e0d4695fe2b0f72a0c33224
Nasim Mahmud <gen.lee.shin@gmail.com>
2017-05-05 17:21:20 +0100

    refactored db

--- index.js
+++ index.js
@@ -3,27 +3,7 @@ const config = require('./config'),
 	app = require('./App'),
 	db = require('./db/db');
 	
-/**
-	++ insert products
-		++ on exists update timestamp
-	++ get stores that are not updated today
-	++ update store timestamp once crawling done
-	++ get stores and opt from db
-	++ add eventing on new data
-	-- database
-		++ store active, deactive, if active then crawl
-		++ url active, deactive, if active then crawl
-		++ product sku is unique
-		++ if sku exists update price and timestamp
-		-- on new set active to no
-	-- image
-		-- get all new products and store their image
 
-	-- detect errors, e.g: site is down or ip blocked
-
-*/
-
-//catch any event errors
 
 process.on('uncaughtException', (err) => {
 
@@ -35,32 +15,4 @@ process.on('uncaughtException', (err) => {
 event.register(config.listeners);
 
 app.start();
-
-// let data = [
-	
-// 	{
-// 		store_id: 1,
-// 		sku: '1-1',
-// 		title: 'Denit Shirt XS'
-// 	},
-
-// 	{
-// 		store_id: 1,
-// 		sku: '1-2',
-// 		title: 'Blue Jeans 24'
-// 	},
-
-// 	{
-// 		store_id: 2,
-// 		sku: '2-1',
-// 		title: 'Dress 1'
-// 	},
-
-// 	{
-// 		store_id: 2,
-// 		sku: '2-2',
-// 		title: 'Skirt 2'
-// 	}
-
-// ];
 
